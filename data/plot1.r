87d75f0010a03d58e59791ef0cee7f528ff0b79b
Lola Quiroga <lolaquiroga25@gmail.com>
2024-04-26 16:38:52 -0700

    added plotgit add plot1.r!

--- data/plot1.r
+++ data/plot1.r
@@ -1,12 +1,23 @@
-# plotting the rate of increase in homelessness in santa cruz county from 2008 - ?
-data=read.csv('homelessness_edited.csv')
+# install.packages("tidyverse")
+# install.packages("ggplot2")
+# install.packages("dplyr")
 
-install.packages("tidyverse")
-install.packages("ggplot2")
-#library(tidyverse)
+library(tidyverse)
 
-#plot()
+# data set of homelessness ct in CA
+unsheltered_data <- read.csv('C:/Users/Lola/Desktop/STAT80B_Project/final-project/data/homelessness_edited.csv')
 
-county = 'Santa Cruz'
+# filter the data for Santa Cruz County, CA
+santa_cruz_data <- unsheltered_data %>%
+  filter(county == "Santa Cruz" & state_name == "California")
 
-state_name = 'California'
+# select the columns for unsheltered homeless counts from 2007 to 2017
+santa_cruz_counts <- select(santa_cruz_data, starts_with("unsheltered_homeless_"))
+
+# reshape the data for plotting
+santa_cruz_counts_long <- pivot_longer(santa_cruz_counts, cols = everything(), names_to = "Year", values_to = "Count")
+
+# plot data!!!
+ggplot(santa_cruz_counts_long, aes(x = as.integer(sub("unsheltered_homeless_", "", Year)), y = Count)) +
+  geom_line() +
+  labs(x = "Year", y = "Unsheltered Homeless Count", title = "Unsheltered Homelessness in Santa Cruz County (2007-2017)") + scale_x_continuous(breaks = c(2007, 2010, 2012, 2015, 2017), labels = c(2007, 2010, 2012, 2015, 2017))
