939449756f6001ee4642515415374b297674d5b9
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-04-26 15:00:40 -0700

    updated plot1.r

--- data/plot1.r
+++ data/plot1.r
@@ -1,1 +1,12 @@
 # plotting the rate of increase in homelessness in santa cruz county from 2008 - ?
+data=read.csv('homelessness_edited.csv')
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+#library(tidyverse)
+
+#plot()
+
+county = 'Santa Cruz'
+
+state_name = 'California'
